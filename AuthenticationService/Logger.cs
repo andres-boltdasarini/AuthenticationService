67cc130f49355e2ccb255fc110ba7758ace5c422
Андрей <lau47@mail.ru>
2025-10-11 12:09:14 +0300

    Добавьте файлы проекта.

---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthenticationService
+{
+    public class Logger
+    {
+
+        public void WriteEvent(string eventMessage)
+        {
+            Console.WriteLine(eventMessage);
+        }
+
+        public void WriteError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+        }
+
+    }
+}
