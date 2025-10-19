af965ef29edbe89c7e74f3831fdd9bef07e10d21
Андрей <lau47@mail.ru>
2025-10-19 18:24:44 +0300

    ExceptionHandler

--- AuthenticationService/Controllers/UserController.cs
+++ AuthenticationService/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using System.Security.Authentication;
 
 namespace AuthenticationService.Controllers
 {
+    [ExceptionHandler]
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
---
+++ AuthenticationService/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AuthenticationService
+{
+    public class ExceptionHandler: ActionFilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context) {
+            string message = "Произошла непредведиенная ошибка";
+
+            if (context.Exception is CustomException)
+            {
+                message = context.Exception.Message;
+            }
+            context.Result = new BadRequestObjectResult(message);
+        }
+    }
+}
